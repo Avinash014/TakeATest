ee4a917de9fa33f026986bf199e7cd8ea22e6af0
Avinash <avinash.kumar@cognizer.ai>
2021-03-15 00:54:47 +0530

    adding material-Ui and designing login page

--- src/Views/Login.tsx
+++ src/Views/Login.tsx
@@ -1,19 +1,57 @@
 import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
 // import logo from "./logo.svg";
 import "../Styles/Login.css";
+import { Button, TextField } from "@material-ui/core";
 type Props = {
   setIsAuthenticated: (value: boolean) => void;
 };
 const Login: React.FC<Props> = ({ setIsAuthenticated }) => {
+  const classes = useStyles();
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleLogin = () => {
+    if (username === "Admin" && password === "Admin") setIsAuthenticated(true);
+    else alert("wrong username or password");
+  };
+
   return (
     <div className="container">
-      <div className="login-form">
-        <div>Username</div>
-        <div>Password</div>
-        <button onClick={() => setIsAuthenticated(true)}>Log In</button>
-      </div>
+      <form className={classes.root} noValidate autoComplete="off">
+        <div className="login-form">
+          <TextField
+            id="standard-search"
+            label="Username"
+            type="search"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button autoFocus onClick={handleLogin} style={{ color: "blue" }}>
+            Log In
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
 
 export default Login;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+  })
+);
